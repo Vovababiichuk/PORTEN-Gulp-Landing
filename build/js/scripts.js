201f4adbd0b19a1fada74e213070f8035f4eaf1d
VVVV BBBB <Volodymyr>
2024-03-06 12:07:11 +0200

    added scrollbar styles

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,28 +1,37 @@
 // Custom Scripts
-// Мобильное меню бургер
 function burgerMenu() {
-    const burger = document.querySelector('.burger')
-    const menu = document.querySelector('.menu')
-    const body = document.querySelector('body')
-    burger.addEventListener('click', () => {
-        if (!menu.classList.contains('active')) {
-            menu.classList.add('active')
-            burger.classList.add('active')
-            body.classList.add('locked')
-        } else {
-            menu.classList.remove('active')
-            burger.classList.remove('active')
-            body.classList.remove('locked')
-        }
-    })
-    // Вот тут мы ставим брейкпоинт навбара
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 991.98) {
-            menu.classList.remove('active')
-            burger.classList.remove('active')
-            body.classList.remove('locked')
-        }
-    })
+	const burger = document.querySelector('.burger')
+	const menu = document.querySelector('.menu')
+	const body = document.querySelector('body')
+	burger.addEventListener('click', () => {
+		if (!menu.classList.contains('active')) {
+			menu.classList.add('active')
+			burger.classList.add('active')
+			body.classList.add('locked')
+		} else {
+			menu.classList.remove('active')
+			burger.classList.remove('active')
+			body.classList.remove('locked')
+		}
+	})
+	window.addEventListener('resize', () => {
+		if (window.innerWidth > 991.98) {
+			menu.classList.remove('active')
+			burger.classList.remove('active')
+			body.classList.remove('locked')
+		}
+	})
 }
 burgerMenu()
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('navbar');
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 10) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+});
 
